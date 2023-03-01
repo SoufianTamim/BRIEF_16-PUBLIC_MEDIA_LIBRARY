5b9af1fa7ab14ea2b796e3e608b0dc8cac8e3254
soufian Tamim <soufiantamim22@gamil.com>
2023-03-01 20:39:32 +0100

    initial commit

--- includes/navbar.php
+++ includes/navbar.php
@@ -3,19 +3,19 @@
   <head>
     <title>Header</title>
     <meta charset="utf-8">
-    <link href="/css/style.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   </head>
-  <body class="d-flex h-100 text-center text-white">
+    <body class="d-flex h-100 text-center text-white bg-dark">
     <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
       <header class="mb-auto">
         <div>
-          <h3 class="float-md-start mb-0"><img src="/images/logo.png"  alt="logo" width="150"></h3>
+          <h3 class="float-md-start mb-0">MediaLib</h3>
           <nav class="nav nav-masthead justify-content-center float-md-end">
-            <a class="nav-link text-white active" aria-current="page" href="#">HOME</a>
-            <a class="nav-link text-white" href="#">LIBRARY</a>
-            <a class="nav-link text-white" href="#"><img src="/images/user.png" alt="profile logo" width="30"></a>
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link" href="#">Features</a>
+            <a class="nav-link" href="#">Contact</a>
           </nav>
         </div>
       </header>
