f3b8cda78e512fc612900bc520b02ff3640b1fed
Soufian Tamim <soufiaintamim22@gmail.com>
2023-03-07 13:32:27 +0100

    initial commit

--- pages/home.php
+++ pages/home.php
@@ -11,6 +11,20 @@
   $category = $crud->read($table_name);
 
 
+if (isset($_GET['find'])) {
+
+  $table_name = "item";
+  $Column_1 = $_GET['Title'];
+  $Column_2 =  $_GET['Author_Name'];
+  $Column_3 = isset($_GET['Category']) ? $_GET['Category'] : null;
+  
+  if ($Column_1 || $Column_2 || $Column_3) {
+    $Filters = $crud->filterItems($table_name, $Column_1, $Column_2, $Column_3);
+    
+    // print_r($Filters);
+  }
+} else {
+
   $table1_name="Item";
   $table2_name="Category";
   $table1_id = "Category_Code";
@@ -18,10 +32,9 @@
 
   $All_Data = $crud-> join($table1_name, $table2_name, $table1_id, $table2_id);
 
+}
 
-  print_r($allData);
-
-  ?>
+?>
 
 
 
@@ -78,20 +91,22 @@
           <div class="underline mx-auto mt-3"></div>
         </div>
       </div>
+
+      <form action="" method="GET">
       <div class="form row">
         <div class="col-lg-3">
           <div class="input-icon-wrap">
-            <input type="text" class="form-control" placeholder="Book Title ...">
+            <input type="text" class="form-control" name="Title" placeholder="Item Title ...">
           </div>
         </div>
         <div class="col-lg-3">
           <div class="input-icon-wrap">
-            <input type="text" class="form-control" placeholder="Author Name ...">
+            <input type="text" class="form-control" name="Author_Name" placeholder="Author Name ...">
           </div>
         </div>
         <div class="col-lg-3">
           <div>
-            <select class="form-select">
+            <select class="form-select" name="Category">
               <option  disabled selected value=""><-- Choose a Category --> </option>
               <?php foreach ($category as $key => $val) { ?>
                   <option value="<?php echo $val['Category_Name']; ?>"><?php echo $val['Category_Name']; ?></option>
@@ -100,15 +115,42 @@
           </div>
         </div>
         <div class="col-lg-3">
-              <button class="btn btn-primary btn-block w-100">Find </button>
-            </div>
-          </div>
-
-          
-          
+              <button class="btn btn-primary btn-block w-100" name="find">Find </button>
+        </div>
+      </div>
+    </form>
           <div class = "d-flex flex-row flex-wrap justify-content-center mt-5">
-          <?php foreach ($All_Data as $key => $val) { ?>
-            <div class=" w-25 bg-dark p-2 rounded-2 m-1">
+          <?php 
+         if (!isset($_GET['find'])) {
+              foreach($All_Data as $key => $val) {  ?>
+            <div class="d-flex flex-wrap  bg-dark p-2 rounded-2 m-1" id="card">
+              <div class = "product-img cont image " >
+                  <img src = "../<?php echo $val['Cover_Image'] ?>" id="image" class = "image img-fluid d-block mx-auto" height="400px !important">
+                  <div class="overlay w-100">
+                     <div class="middle">
+                      <div class="text w-100">
+                        <p><?php echo $val['Author_Name'] ?></p>
+                        <p><?php echo $val['State'] ?></p>
+                        <p><?php echo $val['Edition_Date'] ?></p>
+                        <p><?php echo $val['Category_Name'] ?></p>
+                        <p><?php echo $val['Category_Type'] ?></p>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+              <div class = "product-content text-center d-flex flex-column d-block m-auto mb-3">
+                  <span class = " m-1 text-uppercase"><?php echo $val['Title'] ?></span>
+                  <span class = " m-1 text-success"><?php echo $val['Status'] ?></span>
+                  <button type="button" <?php if(!isset($val['Status']) || $val['Status'] !== "Available") { echo "hidden"; } ?> class="btn btn-outline-primary text-white" data-bs-toggle="modal" data-bs-target="#exampleModal">reserve</button>
+              </div>
+            </div>
+           <?php 
+           }
+            
+          } else if (isset($_GET['find'])) { //check if the method is po  st
+              foreach($Filters as $key => $val) { //loops in the table of announce with filterd data and display the in cards 
+                      ?>
+            <div class="d-flex flex-wrap  bg-dark p-2 rounded-2 m-1" id="card">
               <div class = "product-img cont image " >
                   <img src = "../<?php echo $val['Cover_Image'] ?>" id="image" class = "image img-fluid d-block mx-auto" height="400px !important">
                   <div class="overlay w-100">
@@ -130,9 +172,7 @@
 
               </div>
             </div>
-
-           <?php } ?>
-
+           <?php  } } ?>
 
 
 
