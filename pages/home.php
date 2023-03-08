b6aa23e01b6c1c754f857a14e0d19b62a0620e14
Soufian Tamim <soufiaintamim22@gmail.com>
2023-03-08 13:17:56 +0100

    initial commit

--- pages/home.php
+++ pages/home.php
@@ -1,58 +1,7 @@
 <?php
-// require __DIR__.'/../vendor/autoload.php';
-
-// // include '../php/CRUD.php';
-// include '../php/USER.php';
-
-// $conn = new Database('localhost', 'Library', 'root', '');
-// $user = new User($conn);
-
-// $crud = new Crud($conn);
-
-// $table_name = "Category";
-// $where = "";
-// $category = $crud->read($table_name);
-
-// if (isset($_GET['find'])) {
-//   $table_name = "item";
-//   $Column_1 = $_GET['Title'];
-//   $Column_2 = $_GET['Author_Name'];
-//   $Column_3 = isset($_GET['Category']) ? $_GET['Category'] : null;
-  
-//   if ($Column_1 || $Column_2 || $Column_3) {
-//     $Filters = $crud->filterItems($table_name, $Column_1, $Column_2, $Column_3);
-
-//     // print_r($Filters);
-//   }
-// } else {
-
-//   $table1_name = "Item";
-//   $table2_name = "Category";
-//   $table1_id = "Category_Code";
-//   $table2_id = "Category_Code";
-
-//   $All_Data = $crud->join($table1_name, $table2_name, $table1_id, $table2_id);
-// }
-// $isAuthenticated = $user->isAuthenticated();
-// $userData = $isAuthenticated ? $user->getUser() : null;
-
-// if (isset($_GET['logout'])) {
-//   $user->logout();
-//   header('Location: ../index.php');
-//   exit;
-// }
-
-// if (!$isAuthenticated):
-//   header('Location: ../index.php');
-// else:
-
-//   if ($user->isAdmin()) {
-//     echo "<p>You are an admin!</p>";
-//   } else {
-//     echo "<p>You are not an admin.</p>";
-//   }
 
 ?>
+
 <?php include '../includes/navbar-home.php'; ?>
   <section id="sec-2">
     <div class="container">
--- pages/profile.php
+++ pages/profile.php
@@ -2,19 +2,46 @@
 include '../includes/navbar.php';
 
 
-
+$nickname = $_SESSION['user_id'];
 $table_name = "members";
-$where = "";
-$members = $crud->read($table_name);
+$where = "Nickname = '$nickname'";
+$member = $crud->read($table_name , $where);
 
-print_r($members);
+
+
+
+// $_SESSION['user_id'] = $nickname;
+
+
+
+
+// Get the form data
+$data = array(
+    'Nickname' => $_GET['Nickname'],
+    'Email' => $_GET['Email'],
+    'Phone' => $_GET['Phone'],
+    'CIN' => $_GET['CIN'],
+    'Address' => $_GET['Address']
+);
+$id = 'Nickname';
+
+// Call the update() function
+if($database->update('members', $id, $data)) {
+    // Redirect the user to the page where the updated data is displayed
+    header('Location: display_data.php');
+    exit();
+} else {
+    echo "Error updating data.";
+}
 
 
 
 ?>
 
 
+<?php foreach ($member as $key => $val) { ?>
         <section style="background-color: #eee;">
+        <form method="GET">
           <div class="container py-5 text-black">
             <div class="row">
               <div class="col-lg-4">
@@ -22,25 +49,26 @@ print_r($members);
                   <div class="card-body text-center">
                     <img src="../images/product_img_8.jpg" alt="avatar"
                       class="rounded-circle img-fluid" style="width: 150px;">
-                    <h5 class="my-3 text-black">John Smith</h5>
-                    <p class="text-muted mb-1">Occupation </p>
-                    <p class="text-muted mb-1">birthdate dd/mm/yy </p>
+                      <h5 type="text"class=" text-black text-center  my-2"><?php echo $val['Full_Name']; ?></h5>
+                      <p type="text"class=" text-black text-center  my-2"><?php echo $val['Occupation']; ?></p>
+                      <p type="text"class=" text-black text-center  my-2"><?php echo $val['Birth_Date']; ?></p>
+
                     <div class="d-flex justify-content-center mb-2">
-                      <button type="button" class="btn btn-primary">Edit Profile</button>
-                      <!-- <button type="button" class="btn btn-danger ms-1">Delete Account</button> -->
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-              <div class="col-lg-8">
-                <div class="card mb-4">
+                <div class="col-lg-8">
+                  <div class="card mb-4">
                   <div class="card-body">
                     <div class="row">
                       <div class="col-sm-3">
                         <p class="mb-0">Nick Name</p>
+                      
                       </div>
                       <div class="col-sm-9">
-                        <p class="text-muted mb-0">Johnatan Smith</p>
+                    <input type="text" class=" text-black text-center form-control w-75" name="Nickname"  value="<?php echo $val['Nickname']; ?>">
+
                       </div>
                     </div>
                     <hr>
@@ -49,7 +77,8 @@ print_r($members);
                         <p class="mb-0">Email</p>
                       </div>
                       <div class="col-sm-9">
-                        <p class="text-muted mb-0">example@example.com</p>
+                    <input type="text" class=" text-black text-center form-control w-75" name="Email" value="<?php echo $val['Email']; ?>">
+
                       </div>
                     </div>
                     <hr>
@@ -58,7 +87,8 @@ print_r($members);
                         <p class="mb-0">Phone</p>
                       </div>
                       <div class="col-sm-9">
-                        <p class="text-muted mb-0">(097) 234-5678</p>
+                    <input type="text" class=" text-black text-center form-control w-75" name="Phone" value="<?php echo $val['Phone']; ?>">
+
                       </div>
                     </div>
                     <hr>
@@ -67,7 +97,8 @@ print_r($members);
                         <p class="mb-0">C.I.N</p>
                       </div>
                       <div class="col-sm-9">
-                        <p class="text-muted mb-0">KB 2343443</p>
+                    <input type="text " class=" text-black text-center form-control w-75" name="CIN" value="<?php echo $val['CIN']; ?>">
+
                       </div>
                     </div>
                     <hr>
@@ -76,11 +107,16 @@ print_r($members);
                         <p class="mb-0">Address</p>
                       </div>
                       <div class="col-sm-9">
-                        <p class="text-muted mb-0">Bay Area, San Francisco, CA</p>
+                    <input type="text" class=" text-black text-center form-control w-75" name="Address" value="<?php echo $val['Address']; ?>">
+
                       </div>
                     </div>
+                    <button type="submit" class="btn btn-primary my-2">Save</button>
                   </div>
                 </div>
+                
+                </form>
+
                 <div class="d-flex flex-row">
                   <div class="card w-50 me-2">
                     <div class="card-body">
@@ -95,8 +131,7 @@ print_r($members);
                     </div>
                 </div>
         </section>
-
-        
+        <?php } ?>
     <?php require '../includes/footer.php';?>
     </div>
 
